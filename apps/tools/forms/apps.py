867b02ea05a3e37b1285feb0462122bd7c3e1eaa
Foster Boadi <102637661+fosterb1@users.noreply.github.com>
2026-01-13 16:13:15 +0000

    Fix duplicate app label - add unique label 'tools_forms'
    - Added label = 'tools_forms' to FormsConfig
    - Prevents ImproperlyConfigured error about duplicate 'forms' app
    - Resolves deployment crash on Render

--- apps/tools/forms/apps.py
+++ apps/tools/forms/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class FormsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tools.forms'
+    label = 'tools_forms'  # Unique app label to avoid conflicts
     verbose_name = 'Forms & Surveys'
     
     def ready(self):
